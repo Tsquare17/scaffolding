092f80847d33b001388d34d70b6829831fa9cc04
Trevor Thompson <tsquare1717@gmail.com>
2020-10-19 23:51:02 -0400

    Allow overriding the application name.

--- src/App.php
+++ src/App.php
@@ -8,9 +8,9 @@ use Symfony\Component\Console\Application;
 
 class App extends Application
 {
-    public function __construct($version, $templatePath)
+    public function __construct($version, $templatePath, $appName = null)
     {
-        parent::__construct('Scaffolding', $version);
+        parent::__construct($appName ?: 'Scaffolding', $version);
 
         $this->add(new MakeFile($templatePath));
         $this->add(new MakeSet($templatePath));
